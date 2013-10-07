e3ba76593d0d976f21b268015467bf4abe45dbd1
toctan <tianjin.sc@gmail.com>
2013-10-07 19:35:59 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+      echo "Symlinking $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
